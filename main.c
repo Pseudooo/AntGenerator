881735017f25669c6789b2c25ced95ea9ee4c432
pseudo <mitchell.wright877@gmail.com>
2020-02-22 12:05:05 +0000

    Updated Vector References

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
-
 #include <stdio.h>
-#include "vector.h"
+#include "util/vector.h"
 
-int main() {
+// Setup 64^2 binary grid
+unsigned long grid[64];
+
+// Ant structure
+typedef struct {
+    Vector pos;
+    Vector vel;
+} Ant;
+
+int main() {    
 
     
+    
+}
 
+int get_bit(Vector* v) {
+    return grid[v->j] && 1 << v->i != 0;
+}
+
+void check_bit(Vector* v) {
+    grid[v->j] ^= 1 << v->i
 }
